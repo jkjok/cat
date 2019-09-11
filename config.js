03c6bac87b3d1b1f73787dfdffdcee91c2717831
Nisasa <40004401+Nisasa@users.noreply.github.com>
2019-09-11 18:45:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: '858522531:AAEiEGZpysN8xjgxM6QiF7dTc_6Y9PINm-E',
+    token: '660235857:AAHpND3YGg6CnfDXWKcTt6NhT97oZK5B_K8',
     admin: 549073144,
     yandex: 'https://money.yandex.ru/to/999999999999999/',
     qiwi: 'https://qiwi.com/payment/form/99?extra%5B%27account%27%5D=79999999999&amountInteger=',
